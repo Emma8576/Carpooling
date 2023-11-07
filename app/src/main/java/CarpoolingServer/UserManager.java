66e905bbd7abcd75ebbc3803a25a1861917c11fb
oreoavt <bryanmongenavarro1@gmail.com>
2023-11-06 21:47:02 -0600

    activity_drivel_main.xml created

--- app/src/main/java/CarpoolingServer/UserManager.java
+++ app/src/main/java/CarpoolingServer/UserManager.java
@@ -1,4 +1,15 @@
 package CarpoolingServer;
 
-public class UserManager {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.carpooling.carpooling.R;
+
+public class UserManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drivel_main);
+    }
 }
