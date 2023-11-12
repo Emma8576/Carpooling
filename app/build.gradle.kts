3ad661e1eedd2cfac04a3f3e050c4313060e4985
Emmanuel Calvo <114296940+Emma8576@users.noreply.github.com>
2023-11-11 22:27:41 -0600

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,10 @@ android {
 
     defaultConfig {
         applicationId = "com.carpooling.carpooling"
-        minSdk = 26
-        targetSdk = 33
+        minSdk = 33
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -33,7 +31,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/EmployeeApp/EmployeeMainActivity.java
+++ app/src/main/java/EmployeeApp/EmployeeMainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import com.carpooling.carpooling.R;
 
 public class EmployeeMainActivity extends AppCompatActivity {
-    private static final String BASE_URL = "http://10.0.2.2:123456/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
