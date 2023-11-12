9e936a899689abcb4cb872ea9e5f1c4b0c1b5f0c
Emmanuel Calvo <114296940+Emma8576@users.noreply.github.com>
2023-11-12 09:42:30 -0600

    Se agrega la interfaz inicial de inicio de sesión

--- app/src/main/java/EmployeeApp/EmployeeMainActivity.java
+++ app/src/main/java/EmployeeApp/EmployeeMainActivity.java
@@ -1,17 +1,14 @@
 package EmployeeApp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.carpooling.carpooling.R;
 
 public class EmployeeMainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_main);
-
-
     }
 }
